cc71eb08eb4dfaa577c28c88c51dd89b048a1fb4
Norbert <sadawa.norbert@gmail.com>
2022-11-07 21:36:19 +0100

    added front page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -2,7 +2,22 @@ import React from "react";
 import "./Home.css";
 
 const Home = () => {
-  return <div className="home-container">Home</div>;
+  return (
+    <section className="home-container">
+      <div className="home-info">
+        <div>
+          <h1 className="home-header">
+            Lorem ipsum dolor sit amet consectetur
+          </h1>
+          <span className="home-description">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Veritatis
+          </span>
+        </div>
+
+        <button className="home-learn-more">Dowiedz się więcej</button>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
