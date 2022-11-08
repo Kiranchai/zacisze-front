6a09d76da55dde8eb6ae64bc37dfbb3a4bd4ce2a
Norbert <sadawa.norbert@gmail.com>
2022-11-08 14:56:28 +0100

    small fix

--- src/pages/Rooms.tsx
+++ src/pages/Rooms.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Rooms = () => {
-  return <div>Rooms</div>;
+  return (
+    <div>
+      <ul>
+        <li>
+          <NavLink to="/rooms/1">Room 1</NavLink>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Rooms;
